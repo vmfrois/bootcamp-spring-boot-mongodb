1afa51704c742cd1f3166cf243f60ba7e67c9fb0
Vinicius Frois <viniciusfroism@gmail.com>
2021-01-26 17:01:18 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/viniciusfrois/sbmongo/BootcampSbMongoApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusfrois.sbmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSbMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSbMongoApplication.class, args);
+	}
+
+}
