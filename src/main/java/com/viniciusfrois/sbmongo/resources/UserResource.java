cc0843244f5e230010ad014399cb39b93c1da2db
Vinicius Frois <viniciusfroism@gmail.com>
2021-02-01 15:14:04 -0300

    Utilizando o metodo GET para buscar o usuario por id

--- src/main/java/com/viniciusfrois/sbmongo/resources/UserResource.java
+++ src/main/java/com/viniciusfrois/sbmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,10 +25,17 @@ public class UserResource {
 	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-		/* explicando a expressao lambda = para cada objeto ' x ' da minha lista original, 
-			ele vai retornar um new UserDTO() passando o ' x ' como argumento
-			obs: na classe UserDTO passamos a entidade User como parametro do construtor
+		/* explicando a expressao lambda = para cada objeto 'x' da minha lista original, 
+			ele vai retornar um new UserDTO() passando o 'x' como argumento
+			obs: na classe UserDTO passamos a entidade User como parametro no construtor
 		*/
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	@RequestMapping(value ="/{id}",method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
 }
--- src/main/java/com/viniciusfrois/sbmongo/services/UserService.java
+++ src/main/java/com/viniciusfrois/sbmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.viniciusfrois.sbmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.viniciusfrois.sbmongo.domain.User;
 import com.viniciusfrois.sbmongo.repository.UserRepository;
+import com.viniciusfrois.sbmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
 	}
 
 }
