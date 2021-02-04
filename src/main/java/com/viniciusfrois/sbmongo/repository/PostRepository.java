8aa2b0f46b454039f884f3e0692e07b525014f9f
Vinicius Frois <viniciusfroism@gmail.com>
2021-02-04 19:21:42 -0300

    Consulta simples com @Query

--- src/main/java/com/viniciusfrois/sbmongo/repository/PostRepository.java
+++ src/main/java/com/viniciusfrois/sbmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.viniciusfrois.sbmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.viniciusfrois.sbmongo.domain.Post;
@@ -10,6 +11,9 @@ import com.viniciusfrois.sbmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
 
+	@Query("{ 'title': { $regex: ?0 , $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
